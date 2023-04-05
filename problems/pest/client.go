3453bfa584b87f1a3fc5112c4ad2abcd138f269c
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2023-04-05 12:47:44 -0700

    rearrange some hello

--- problems/pest/client.go
+++ problems/pest/client.go
@@ -20,20 +20,36 @@ func init() {
 	helloPayload, _ = (&protocol.Hello{Protocol: "pestcontrol", Version: 1}).Serialize()
 }
 
+func (c *client) sendMessage(msg protocol.Message) error {
+	payload, err := msg.Serialize()
+	if err != nil {
+		log.Error().Type("message_type", msg).Err(err).Msg("could not serialize message")
+		return err
+	}
+
+	_, err = c.conn.Write(payload)
+	if err != nil {
+		log.Error().Err(err).Msg("could not write response")
+		return err
+	}
+
+	return nil
+}
+
 func (c *client) handleConnection() {
 	defer c.conn.Close()
 
-	firstMsg, err := protocol.Deserialize(c.conn)
-	_, herr := c.conn.Write(helloPayload)
-	if herr != nil {
-		log.Fatal().Err(err).Msg("could not write hello to client")
+	_, err := c.conn.Write(helloPayload)
+	if err != nil {
+		log.Error().Err(err).Msg("could not write hello to client")
+		return
 	}
 	log.Debug().Msg("wrote hello")
+
+	firstMsg, err := protocol.Deserialize(c.conn)
 	if err != nil {
 		log.Error().Err(err).Msg("error reading hello from client")
-		resp := &protocol.Error{Message: fmt.Sprintf("error: %s", err.Error())}
-		respBytes, _ := resp.Serialize()
-		c.conn.Write(respBytes)
+		c.sendMessage(&protocol.Error{Message: fmt.Sprintf("could not deserialize: %s", err.Error())})
 		return
 	}
 
@@ -41,16 +57,14 @@ func (c *client) handleConnection() {
 	if !ok {
 		log.Warn().Type("message_type", firstMsg).Msg("non-hello got as first message")
 		resp := &protocol.Error{Message: "non-hello message as preamble"}
-		respBytes, _ := resp.Serialize()
-		c.conn.Write(respBytes)
+		c.sendMessage(resp)
 		return
 	}
 
 	if hi.Protocol != "pestcontrol" || hi.Version != 1 {
 		log.Warn().Str("protocol", hi.Protocol).Uint32("version", hi.Version).Msg("invalid protocol or version")
-		resp := &protocol.Error{Message: "non-hello message as preamble"}
-		respBytes, _ := resp.Serialize()
-		c.conn.Write(respBytes)
+		resp := &protocol.Error{Message: "invalid protocol or version"}
+		c.sendMessage(resp)
 		return
 	}
 
@@ -59,8 +73,8 @@ func (c *client) handleConnection() {
 		msg, err = protocol.Deserialize(c.conn)
 		if err != nil {
 			log.Error().Err(err).Msg("could not read message in loop")
-			resp, _ := (&protocol.Error{Message: "invalid message"}).Serialize()
-			c.conn.Write(resp)
+			resp := &protocol.Error{Message: fmt.Sprintf("invalid message: %s", err.Error())}
+			err = c.sendMessage(resp)
 			return
 		}
 
@@ -70,16 +84,17 @@ func (c *client) handleConnection() {
 			if err != nil {
 				log.Warn().Err(err).Uint32("site", m.Site).Msg("invalid observation")
 				resp := &protocol.Error{Message: err.Error()}
-				respBytes, _ := resp.Serialize()
-				c.conn.Write(respBytes)
-				continue
+				c.sendMessage(resp)
+				return
 			}
 			c.hub.visitChan <- &Visit{
 				Site:         m.Site,
 				Observations: obs,
 			}
 		default:
-			log.Fatal().Type("message_type", m).Msg("invalid type gotten from client")
+			c.sendMessage(&protocol.Error{Message: "invalid message type"})
+			log.Error().Type("message_type", m).Msg("invalid type gotten from client")
+			return
 		}
 	}
 }
